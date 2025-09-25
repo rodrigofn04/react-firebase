bf78b233faf8a98d616bd255c90b7f60c8267637
Rodrigo <rodrigo_ferreira20.04@hotmail.com>
2025-09-24 21:58:34 -0300

    hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
 import Login from "./pages/Login.jsx";
@@ -7,19 +7,15 @@ import Register from "./pages/Register.jsx";
 import Principal from "./pages/Principal.jsx";
 import "./App.css";
 
-// O componente App.jsx agora gerencia o estado de autenticação globalmente
 const App = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Adiciona o listener para o estado de autenticação
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       setLoading(false);
     });
-
-    // Função de limpeza
     return () => unsubscribe();
   }, []);
 
@@ -32,15 +28,14 @@ const App = () => {
   }
 
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/login" element={!user ? <Login /> : <Navigate to="/principal" />} />
         <Route path="/register" element={!user ? <Register /> : <Navigate to="/principal" />} />
-        {/* A rota principal agora é protegida */}
         <Route path="/principal" element={user ? <Principal /> : <Navigate to="/login" />} />
         <Route path="*" element={<Navigate to={user ? "/principal" : "/login"} />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 

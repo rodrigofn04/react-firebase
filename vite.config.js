2c08015bb97d59c79a8ed2298e03ac7832e0eefe
Rodrigo <rodrigo_ferreira20.04@hotmail.com>
2025-09-24 22:02:48 -0300

    updating host

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/rodrigofn04/react-firebase 
+  base: 'https://github.com/rodrigofn04/react-firebase '
 })

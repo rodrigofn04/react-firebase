b3b263e4e5e2b206463656fb3d7da2f38c25dc93
Rodrigo <rodrigo_ferreira20.04@hotmail.com>
2025-09-24 22:05:28 -0300

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/rodrigofn04/react-firebase '
+  base: '/ir-facil/'
 })
